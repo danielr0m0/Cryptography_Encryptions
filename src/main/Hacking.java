1fe9b525db9d85fec171eaf7c73b7d3701730f02
DanielrR0m0 <drdanielromo@gmail.com>
2018-10-29 21:49:53 -0700

    finish project... it takes 4 min to fully to run

--- src/main/Hacking.java
+++ src/main/Hacking.java
@@ -9,15 +9,6 @@ public class Hacking {
     public static void main(String[] args) throws IOException {
         String plainText = "CRYPTOGRAPHY";
         byte[] key = new byte[]{0,1,1,1,0,0,1,1,0,1};
-        //help to understand code for cascii
-        String f = plainText.charAt(0)+"";
-        //to get 8 bit representation of a letter
-        print(CASCII.Convert(f));
-        //to convert bit[] to a string
-        System.out.println("\n"+CASCII.toString(CASCII.Convert(f)));
-
-        //pass the plainText string to CASCII convert class where it converts the plain text to byte [] and add padding to the end to make the length divisible by 8
-        print(CASCII.Convert(plainText));
 
         //need to split the converted plaintext into 8 bits and pass them in encrypt
         byte[] encrypt = CASCII.Convert(plainText);
@@ -28,16 +19,15 @@ public class Hacking {
         byte[] answer1= encrypt(key,encrypt);
         print(answer1);
 
-        //test if i decrypt the answer i got for encryption will give me back the plain text
-        System.out.println("\n decrypt the answer i got from above ^ = ");
-        System.out.println(CASCII.toString(decrypt(key,answer1)));
+        System.out.println();
 
         System.out.println("length of answer1: "+ answer1.length);
 
+
         String text2="1011011001111001001011101111110000111110100000000001110111010001111011111101101100010011000000101101011010101000101111100011101011010111100011101001010111101100101110000010010101110001110111011111010101010100001100011000011010101111011111010011110111001001011100101101001000011011111011000010010001011101100011011110000000110010111111010000011100011111111000010111010100001100001010011001010101010000110101101111111010010110001001000001111000000011110000011110110010010101010100001000011010000100011010101100000010111000000010101110100001000111010010010101110111010010111100011111010101111011101111000101001010001101100101100111001110111001100101100011111001100000110100001001100010000100011100000000001001010011101011100101000111011100010001111101011111100000010111110101010000000100110110111111000000111110111010100110000010110000111010001111000101011111101011101101010010100010111100011100000001010101110111111101101100101010011100111011110101011011";
-        System.out.println();
+        System.out.println("to decrypt:");
         System.out.println(text2);
-        System.out.println(text2.length());
+
         int count = 0;
 
         for (int i = 0; i < Math.pow(2, 10); i++) {
@@ -54,41 +44,72 @@ public class Hacking {
             int repeatsQ= data.length() - data.replace("Q","").length();
             int repeatsX= data.length() - data.replace("X","").length();
             int repeatsZ= data.length() - data.replace("Z","").length();
+
             if(decrypt[decrypt.length-1]==(byte)0& decrypt[decrypt.length-2]== (byte) 0 & repeatsQ<3 & repeatsX<3 & repeatsZ<3) {
                 print(key1);
                 System.out.print(" ");
                 System.out.print(data);
                 System.out.println("\n");
-                count++;
+
             }
 
         }
 
-        System.out.println(count);
 
         String text3 = "00011111100111111110011111101100111000000011001011110010101010110001011101001101000000110011010111111110000000001010111111000001010010111001111001010101100000110111100011111101011100100100010101000011001100101000000101111011000010011010111100010001001000100001111100100000001000000001101101000000001010111010000001000010011100101111001101111011001001010001100010100000";
         System.out.println(text3);
         System.out.println(text3.length());
         //for triple SDES need to write to text file since results is too much. since their is 1,048,576 results
         //or we need some filter to shorten the results we get.
-//        for (int i = 0; i < Math.pow(2, 10); i++) {
-//            byte[] key1 = byte10BitSize(i);
-//            for (int j = 0; j < Math.pow(2, 10); j++) {
-//                //i for key 1 j for key 2
-//                byte[] key2 = byte10BitSize(j);
-//                byte[] decrypt = decrypt(key1,key2,stringToByteArr(text3));
-//                //print stuff
-//                if(decrypt[decrypt.length-1]==(byte)0 &&  decrypt[decrypt.length-2]== (byte) 0 && decrypt[decrypt.length-3]== (byte) 0 ){
-//                    print(key1);
-//                    System.out.print(" ");
-//                    print(key2);
-//                    System.out.print(" ");
-//                    System.out.print(CASCII.toString(decrypt));
-//                    System.out.println("\n");
-//                }
-//
-//            }
-//        }
+        for (int i = 0; i < Math.pow(2, 10); i++) {
+            byte[] key1 = byte10BitSize(i);
+            for (int j = 0; j < Math.pow(2, 10); j++) {
+                //i for key 1 j for key 2
+                byte[] key2 = byte10BitSize(j);
+                byte[] decrypt = decrypt(key1,key2,stringToByteArr(text3));
+                String data = CASCII.toString(decrypt);
+                //print stuff
+                //if it has more than 2 z
+                //if it has more than 2 x
+                //if it has more than 2 q
+                //if it has more than 2 j
+                //if special characters appear more than times one time in one word
+
+                int repeatsJ= data.length() - data.replace("J","").length();
+                int repeatsQ= data.length() - data.replace("Q","").length();
+                int repeatsX= data.length() - data.replace("X","").length();
+                int repeatsZ= data.length() - data.replace("Z","").length();
+
+                int repeatPeriod = data.length()- data.replace(".","").length();
+                int repeatQuestion = data.length()- data.replace("?","").length();
+                int repeatComa = data.length()- data.replace(",","").length();
+                int repeatColen = data.length()- data.replace(":","").length();
+                int repeatSpecial = data.length()- data.replace("'","").length();
+
+                boolean repeats = false;
+
+                //if there is more than one special characters in one word
+                String datas[] = data.split(" ");
+                for (String s: datas) {
+                    if(repeatPeriod+repeatColen+repeatComa+repeatQuestion+repeatSpecial >2){
+                        repeats= true;
+                        break;
+                    }
+
+                }
+
+                if(decrypt[decrypt.length-1]==(byte)0 &&  decrypt[decrypt.length-2]== (byte) 0 && decrypt[decrypt.length-3]== (byte) 0 && repeatsQ<2 && repeatsX<2 && repeatsZ<2 && repeatsJ<2 && !repeats){
+                    print(key1);
+                    System.out.print(" ");
+                    print(key2);
+                    System.out.print(" ");
+                    System.out.print(data);
+                    System.out.println("\n");
+                }
+
+            }
+        }
+
     }
 
     public static byte[] encrypt(byte[] key, byte[] text){
